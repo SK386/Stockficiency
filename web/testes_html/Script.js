35205ece7dd9f456e442e34da60411bfcba20faa
toddyn_c-137 <dagvjr@gmail.com>
2022-08-27 14:10:18 -0300

    some alterations in the side bar

--- web/testes_html/Script.js
+++ web/testes_html/Script.js
@@ -1,4 +1,4 @@
-function AtualizeGrafs(Option,xValues,yValues){
+function lineGraf(Option,xValues,yValues){
 
 	var xValues = [50,60,70,80,90,100,110,120,130,140,150];
 	var yValues = [7,8,8,9,9,9,10,11,14,14,15];
@@ -25,11 +25,47 @@ function AtualizeGrafs(Option,xValues,yValues){
 
 }
 
- AtualizeGrafs("graf1");
+function pieGraf(Option,){
+
+var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart(Option, {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "World Wide Wine Production 2018"
+    }
+  }
+});
+
+}
+
+lineGraf("graf1");
  
- AtualizeGrafs("graf2");
+pieGraf("graf2");
 
-AtualizeGrafs("graf3");
+lineGraf("graf3");
 
-AtualizeGrafs("graf4");
+lineGraf("graf4");
 
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
