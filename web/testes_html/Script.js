3a2ccdd077ebcfcfa3c2a82a30125a490d1944a6
toddyn_c-137 <dagvjr@gmail.com>
2022-09-03 15:55:17 -0300

    minha vida é um lixo

--- web/testes_html/Script.js
+++ web/testes_html/Script.js
@@ -65,3 +65,6 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+var Grafbtn = document.getElementById("graf2");
+Grafbtn.style("heigth:10%");
