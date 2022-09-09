26fd3d5022c5eddffac5062125e6ccc31aaa595a
GustavoMarinho82 <gustavo.mariinho06@gmail.com>
2022-09-08 22:22:44 -0300

    Renomeado clientes -> empresas, para evitar ambiguidades

--- db/clientes.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE clientes (
-    id_cliente INT(7) AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(50) NOT NULL,
-        PRIMARY KEY (id_cliente)
-);
--- db/clientes_produtos.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE clientes_produtos (
-    cliente_id INT(7) NOT NULL,
-    produto_id INT(7) NOT NULL,
-        FOREIGN KEY (cliente_id) REFERENCES clientes (id_cliente),
-        FOREIGN KEY (produto_id) REFERENCES produtos (id_produto)
-);
