ac8ec35deed5b001508848841a3c38fd808a1fce
toddyn_c-137 <dagvjr@gmail.com>
2022-09-10 19:26:04 -0300

    new page

--- web/testes_html/Script.js
+++ web/testes_html/Script.js
@@ -72,3 +72,5 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+ document.getElementById("table").InnerHTML = " <tr> <td> 1 </td> <td>  teste </td> <td> 100 </td> <td> R$ 300,00 </td> </tr> ";
