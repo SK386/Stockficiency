e64ac47915b6c7daa50bf1ae21c0940c7309e0f3
toddync <dagvjr@gmail.com>
2022-09-16 14:28:18 -0300

    é mesmo

--- web/testes_html/tabela_estoque.js
+++ web/testes_html/tabela_estoque.js
@@ -1,10 +1,27 @@
+let cont = 0;
+let con = 1;
+var name = "Tr";
 
+function addCell(){
+    cont += 1;
 
-var y = document.createElement("TR");
-y.setAttribute("id", "myTr");
-document.getElementById("table").appendChild(y);
+    if (cont<=4) {
 
-var z = document.createElement("TD");
-var t = document.createTextNode("cell");
-z.appendChild(t);
-document.getElementById("myTr").appendChild(z);
+        var y = document.createElement("TR");
+        y.setAttribute("id", (name + con));
+        document.getElementById("table").appendChild(y);
+    
+        var z = document.createElement("TD");
+        var t = document.createTextNode("cell");
+        z.appendChild(t);
+        document.getElementById((name + con)).appendChild(z);
+    
+    } else {
+        
+        cont = 0;
+        con += 1;
+        console.log ((name + con));
+
+    }
+
+}
