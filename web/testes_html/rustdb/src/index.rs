b33f5bbebf13e8fc5caddb538f97b99e0f3ba76b
GustavoMarinho82 <gustavo.mariinho06@gmail.com>
2022-09-28 16:39:05 -0300

    Tirando a coluna 'descrição' de produtos

--- web/testes_html/rustdb/src/index.rs
+++ web/testes_html/rustdb/src/index.rs
@@ -1,30 +1,28 @@
 //PRODUTOS
-pub fn create_p(conn: &mut PooledConn, cod: String, nome: String, desc: Option<String>, estoque: u32, preco: f64) {
-    conn.exec_drop("INSERT INTO produtos (codigo_produto, nome_produto, descricao_produto, qtd_estoque, preco) VALUES (:a, :b, :c, :d, :e)",
+pub fn create_p(conn: &mut PooledConn, cod: String, nome: String, estoque: u32, preco: f64) {
+    conn.exec_drop("INSERT INTO produtos (codigo_produto, nome_produto, qtd_estoque, preco) VALUES (:a, :b, :c, :d)",
         params! {
             "a" => cod,
             "b" => nome,
-            "c" => desc,
-            "d" => estoque,
-            "e" => preco
+            "c" => estoque,
+            "d" => preco
         }
     ).unwrap();
 }
 
-pub fn read_p(conn: &mut PooledConn) -> Vec<(String, String, String, i32, f64)> {
-    let res:Vec<(String, String, String, i32, f64)> = conn.query("SELECT codigo_produto, nome_produto, descricao_produto, qtd_estoque, preco FROM produtos")
+pub fn read_p(conn: &mut PooledConn) -> Vec<(String, String, i32, f64)> {
+    let res:Vec<(String, String, i32, f64)> = conn.query("SELECT codigo_produto, nome_produto, qtd_estoque, preco FROM produtos")
         .unwrap();
 
     res
 }
 
-pub fn update_p(conn: &mut PooledConn, cod: String, nome: String, desc: Option<String>, estoque: u32, preco: f64) {
-    conn.exec_drop("UPDATE produtos SET nome_produto=:b, descricao_produto=:c, qtd_estoque=:d, preco=:e WHERE codigo_produto=:a", params! {
+pub fn update_p(conn: &mut PooledConn, cod: String, nome: String, estoque: u32, preco: f64) {
+    conn.exec_drop("UPDATE produtos SET nome_produto=:b, qtd_estoque=:c, preco=:d WHERE codigo_produto=:a", params! {
         "a" => cod,
         "b" => nome,
-        "c" => desc,
-        "d" => estoque,
-        "e" => preco
+        "c" => estoque,
+        "d" => preco
         }
     ).unwrap();
 }
