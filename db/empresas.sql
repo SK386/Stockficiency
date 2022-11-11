8cf4ceea5456f58a8f13af3b4b85396576e334cb
Gustavo Marinho <gustavo.mariinho06@gmail.com>
2022-11-11 20:49:26 -0300

    Adicionada a tabela encomendas e melhorias
    Adicionado também um arquivo vazio para a futura tabela de finanças.
    
    Melhorias - UNSIGNEDs, INTs -> MEDIUMINT e BIGINT, removido 'nome_empresa' da tabela 'empresas'.

--- db/empresas.sql
+++ db/empresas.sql
@@ -1,7 +1,6 @@
 CREATE TABLE empresas (
-    id_empresa INT(7) AUTO_INCREMENT, #0 000 000
-    nome_empresa VARCHAR(50) NOT NULL,
-    email VARCHAR(65) NOT NULL UNIQUE,
+    id_empresa MEDIUMINT UNSIGNED AUTO_INCREMENT,
+    email VARCHAR(60) NOT NULL UNIQUE,
     senha VARCHAR(50) NOT NULL,
         PRIMARY KEY (id_empresa)
 );
--- db/produtos.sql
+++ db/produtos.sql
@@ -1,8 +1,8 @@
 CREATE TABLE produtos (
-    id_produto INT(7) AUTO_INCREMENT,
+    id_produto INT UNSIGNED AUTO_INCREMENT,
     codigo_produto VARCHAR(9) NOT NULL UNIQUE,	#ABC 000 000
-    nome_produto VARCHAR(50) NOT NULL UNIQUE,
-    qtd_estoque INT(9) NOT NULL,	#000 000 000
+    nome_produto VARCHAR(50) NOT NULL,
+    qtd_estoque MEDIUMINT UNSIGNED NOT NULL,
     preco DECIMAL(9, 2) NOT NULL,	#1234567.89
         PRIMARY KEY (id_produto)
 );
