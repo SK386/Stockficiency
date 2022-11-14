21447c4db3abbc2f45a09e33691fd10da870b88b
toddync <dagvjr@gmail.com>
2022-11-14 16:44:35 -0300

    te odeio Ornelas

---
+++ site/php_arquivos/deletarProduto.php
@@ -0,0 +1,31 @@
+<?php
+include('conexao.php');
+
+    $cod = $_POST['cod'];
+
+    if(strlen($cod) == 0) {
+        echo "Preencha o campo do código!";
+    
+    } else {
+
+        $sql = "SELECT * FROM produtos WHERE codigo_produto='$cod'";
+            $consulta = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($consulta) == 0) {
+            echo "Produto não encontrado!";
+        
+        } else {
+
+            $sql = "DELETE FROM produtos WHERE codigo_produto='$cod'";
+                mysqli_query($mysqli, $sql);
+
+            echo "Produto deletado com sucesso!";
+        }
+    }
+?>
+
+<HTML>
+    <BODY>
+        <p><a href="index.html">Voltar</a>
+    </BODY>
+</HTML>
