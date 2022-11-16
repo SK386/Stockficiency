65d34c31573461d1ea0dc093bb97021a71e325d8
Gustavo Marinho <gustavo.mariinho06@gmail.com>
2022-11-15 21:24:17 -0300

    Adicionadas duas possibilidades de 'finanças'
    1° caso seja só mês e ano.
    2° caso seja dia, mês e ano.

--- db/financas.sql
+++ db/financas.sql
@@ -1,3 +1,20 @@
+/*
 CREATE TABLE financas (
-    
+	mes TINYINT NOT NULL,
+	ano TINYINT NOT NULL,
+	ganhos INT UNSIGNED NOT NULL,
+	despesas INT UNSIGNED NOT NULL,
+  	
+    empresa_id MEDIUMINT UNSIGNED NOT NULL,
+        FOREIGN KEY (empresa_id) REFERENCES empresas (id_empresa)
 );
+
+CREATE TABLE financas (
+	data_ DATE NOT NULL, #'AAAA-MM-DD'
+	ganhos INT UNSIGNED NOT NULL,
+	despesas INT UNSIGNED NOT NULL,
+  	
+    empresa_id MEDIUMINT UNSIGNED NOT NULL,
+        FOREIGN KEY (empresa_id) REFERENCES empresas (id_empresa)
+);
+*/
