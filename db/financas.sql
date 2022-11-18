bde5164c930744b71c9176555b8dfcad046f1ca2
Gustavo Marinho <gustavo.mariinho06@gmail.com>
2022-11-18 20:37:29 -0300

    Update financas.sql

--- db/financas.sql
+++ db/financas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE financas (
     periodo DATE NOT NULL, #'AAAA-MM-DD'
-    ganhos DECIMAL(11, 2) UNSIGNED NOT NULL, #123456789.10
-    despesas DECIMAL(11, 2) UNSIGNED NOT NULL,
+    ganhos DECIMAL(11, 2) NOT NULL, #123456789.10
+    despesas DECIMAL(11, 2) NOT NULL,
 
     empresa_id MEDIUMINT UNSIGNED NOT NULL,
         FOREIGN KEY (empresa_id) REFERENCES empresas (id_empresa)
